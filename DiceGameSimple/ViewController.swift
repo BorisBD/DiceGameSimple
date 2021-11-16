a075f3197b22a3fd9ef844aee01b45d374e08df2
zen <alyxfly@gmail.com>
2021-11-16 19:59:22 +0200

    Initial Commit

---
+++ DiceGameSimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGameSimple
+//
+//  Created by Boris Dudnik on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
